b3f60748fc7ff180ec5be82e06adcaf5c90282b8
jianliang <jianliang@szotzed.net>
2018-05-28 21:58:48 +0800

    20180528

--- work/remote/123.sh
+++ work/remote/123.sh
@@ -1,2 +1,2 @@
-rdesktop 192.168.1.123 -g 1500x800 -u jian1 -p tellmemore -r disk:f=/media/jian/518C138300565C98,g=/
+rdesktop 192.168.1.123 -g 1500x800 -u jian1 -p tellmemore -r disk:f=/media/jian/518C138300565C98,g=/ -k en-us -a16 2>/dev/null
 date
