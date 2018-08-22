96e38f4a9e624770a86b614fdf687bc255171988
jianliang <jianliang@szotzed.net>
2018-08-22 08:37:12 +0800

    20180821

--- python/0keeper.py
+++ python/0keeper.py
@@ -5,23 +5,28 @@ import http.client
 import requests
 import json
 
-recorder_file='0keeper.txt'
-
+recorder_file='0keeper.md'
 
 def mac_isexist(mac):
     f1=file(recorder_file)
+    #print("we are find " + mac)
     for line in f1.readlines():
-        if(line.find(mac)):
+        #print line
+        if(line.find(mac)>0):
             print(mac+" is add before")
             return True
     return False
 
 def addit(cname, mac, ip):
     if(mac_isexist(mac)):
-        return
-    f1=file(recorder_file, 'r+')
-    f1.write(cname + ' | ' +mac + ' | '+ ip)
+        return False
+    f1=file(recorder_file, 'a')
+    for _ in range(55-len(cname)):
+        cname += " "
+    print(mac+"will add")
+    f1.write(cname + ' | ' +mac + ' | '+ ip + '\n')
     f1.close
+    return True
 
 def addhttp1():
    connection = http.client.HTTPConnection('192.168.19.1', 80, timeout=10) 
@@ -31,11 +36,24 @@ def addhttp1():
    connection.close()
 
 def addhttp():
-    json1={"cmd":122,"method":"GET","language":"CN","sessionId":"a608305057107c037c3fa2504dcf81875f4b48f5a49b734207fe3c101c4f5855"}
+    loginpayload={'cmd':100,'method':"POST", 'language':'EN', 'username':'admin', 'passwd':'f6339c4179e7b9b4b7c9da08502184ec', 'sessionId':'a6083050f134e86e79a7af2a5217ab4ea0ff26b8b1a25ea4b45fb969aa49d457' }
+    #test = 'test=test'
+    headers = {'Content-Type' : 'multipart/form-data'}
+    requests.post('http://192.168.19.1/cgi-bin/http.cgi', params=loginpayload, headers=headers)
+    #print(login.json())
+    json1={"cmd":122,"method":"GET","language":"CN","sessionId":"a6083050f134e86e79a7af2a5217ab4ea0ff26b8b1a25ea4b45fb969aa49d457"}
     r = requests.post('http://192.168.19.1/cgi-bin/http.cgi', json=json1)
-    //print(r.status_code)
-    print(r.json())
-
+    #print(r.status_code)
+    macs=r.json()['data']
+    count=0
+    for pc in macs:
+        mac=pc[1].upper()
+        ip=pc[2]
+        pcname=pc[3]
+        if(addit(pcname, mac, ip)):
+            count += 1
+        #print(mac, ip, pcname)
+    print "add ", count 
 
 if __name__ == '__main__':
     if(len(sys.argv) == 1):
