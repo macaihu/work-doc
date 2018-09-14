aa0c7c49911321e5faa230c9da268e4a4eb14aa9
jianliang <jianliang@szotzed.net>
2018-09-14 08:41:46 +0800

    20180914

--- python/memery_leak_check.py
+++ python/memery_leak_check.py
@@ -4,6 +4,9 @@
 
 #pip install paramiko
 import paramiko  
+import time
+import sys
+import os
 	
 def sshclient_execmd(hostname, port, username, password, execmd):  
 	paramiko.util.log_to_file("paramiko.log")  
@@ -14,36 +17,41 @@ def sshclient_execmd(hostname, port, username, password, execmd):
 	stdin.write("Y")  # Generally speaking, the first connection, need a simple interaction.  
 	print stdout.read()  
 	s.close()  
-	
-def check_error(hostname, port, username, password):
-	#paramiko.util.log_to_file("paramiko.log")  
-	s = paramiko.SSHClient()  
-	s.set_missing_host_key_policy(paramiko.AutoAddPolicy())  
-	s.connect(hostname=hostname, port=port, username=username, password=password)  
+
+def read_info(s,hostname):
+	stdin, stdout, stderr = s.exec_command ('uptime')  
+	print stdout.read()
 	stdin, stdout, stderr = s.exec_command ('ls /tmp/coredump')  
 	#stdin.write("Y")  # Generally speaking, the first connection, need a simple interaction.  
 	coredumpfiles = stdout.read() 
 	if(coredumpfiles==""):
 		print "ok no crash files"
 	else:
+		os.system("sshpass -p sztz369147258 scp -P 8357 root@" + hostname + ":/tmp/coredump/* .")
 		print coredumpfiles
 	stdin, stdout, stderr = s.exec_command ('cat /proc/meminfo | grep MemFree')  
-	print stdout.read()
-	stdin, stdout, stderr = s.exec_command ('uptime')  
 	print stdout.read()	
+	stdin, stdout, stderr = s.exec_command ('dmesg -c')  
+	print stdout.read()	
+
+def check_error(hostname, port, username, password):
+	#paramiko.util.log_to_file("paramiko.log")  
+	s = paramiko.SSHClient()  
+	s.set_missing_host_key_policy(paramiko.AutoAddPolicy())  
+	s.connect(hostname=hostname, port=port, username=username, password=password)  
+	while(True):
+		read_info(s, hostname)
+		time.sleep(30)
 	s.close()  	
 
-def main():
-	#hostname = '192.168.19.3'  
+def main(host):
 	port = 8357
 	username = 'root'  
 	password = 'sztz369147258'  
-	execmd = "ls /"  
-	#sshclient_execmd(hostname, port, username, password, execmd)  
-	check_error("192.168.19.1" , port, username, password)  
-	check_error("192.168.19.3" , port, username, password)  
+	check_error(host , port, username, password)  
+	#check_error("192.168.19.3" , port, username, password)  
 
 
 if __name__ == '__main__':
-	main()
+	main(sys.argv[1])
 
