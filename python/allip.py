7603cb736c46daab4d1f36a899ca6ec54daf1269
jianliang <jianliang@szotzed.net>
2018-10-25 08:40:53 +0800

    20181024

--- python/allip.py
+++ python/allip.py
@@ -1,43 +1,20 @@
 #!/usr/bin/python
 
 
-#pip install nmap
-#pip install python-nmap
-#pip install netifaces
-
-import netifaces
-import nmap
-
-
-def get_gateways():
-    return netifaces.gateways()['default'][netifaces.AF_INET][0]
-
-def get_ip_lists(gateway):
-    ip_lists = []
-    for i in range(1, 256):
-        ip_lists.append('{}{}'.format(gateway[:-1], i))
-    return ip_lists
-
-def scan_ip_survial(ip):
-    nmScan = nmap.PortScanner()
-    nmScan.scan(hosts=ip, arguments='-sP')
-    if nmScan[ip]['hostnames'][0]['name']:
-        return {'IP Address:': ip,
-                'Hostname:': nmScan[ip]['hostnames'][0]['name']
-               }
-    else:
-        return None
+import os
 
 def get_all_survial_hosts():
-    survial_hosts = []
-    gateway = get_gateways()
-    ip_lists = get_ip_lists(gateway)
-    for ip in ip_lists:
-        scan_rst = scan_ip_survial(ip)
-        if scan_rst:
-            survial_hosts.append(scan_rst)
-            print(scan_rst)
-    return survial_hosts
+    os.system('mkdir -p hosts')
+    allips = os.popen('nmap -sn 192.168.1.0/24')
+    lines = allips.readlines()
+    for line in lines:
+        if(line.find("report")>0):
+            print line
+            host = line.split()
+            ip = host[4]
+            if not os.path.isfile('hosts/'+ip) :
+                os.system('nmap -A ' + ip + ' >> hosts/'+ ip)
+        #print line
 
 
 if __name__ == '__main__':
