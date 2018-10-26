9fcf9e15dc764b09fdb27c68aabdd7ea22e7f176
jianliang <jianliang@szotzed.net>
2018-10-26 08:30:48 +0800

    20181025

--- python/allip.py
+++ python/allip.py
@@ -2,21 +2,32 @@
 
 
 import os
+import sys
 
-def get_all_survial_hosts():
+def get_all_survial_hosts(ips):
     os.system('mkdir -p hosts')
-    allips = os.popen('nmap -sn 192.168.1.0/24')
+    #allips = os.popen('nmap -sn 192.168.1.0/24')
+    allips = os.popen('nmap -sn ' + ips)
     lines = allips.readlines()
     for line in lines:
         if(line.find("report")>0):
-            print line
+            #print line
             host = line.split()
-            ip = host[4]
+            ip = host[len(host)-1]
+            ip = ip.replace('(', '')
+            ip = ip.replace(')', '')
+            #print ip
             if not os.path.isfile('hosts/'+ip) :
+                print 'checking ' +ip + '.....'
                 os.system('nmap -A ' + ip + ' >> hosts/'+ ip)
+            else:
+                print 'we have checked ' + ip 
         #print line
 
 
 if __name__ == '__main__':
-    get_all_survial_hosts()
+    if len(sys.argv) == 1 :
+        get_all_survial_hosts('192.168.1.0/24')
+    else:
+        get_all_survial_hosts(sys.argv[1])
 
